442757ed172000753f7f94fe4941f2145a68a859
Zhanteng Xie <zhanteng.xie@temple.edu>
2024-12-07 21:22:46 -0500

    Update additional_topic_subscriber.h

--- 2ndparty/spencer_tracking_rviz_plugin/src/additional_topic_subscriber.h
+++ 2ndparty/spencer_tracking_rviz_plugin/src/additional_topic_subscriber.h
@@ -60,7 +60,7 @@ namespace rviz
 /** @brief Helper superclass for AdditionalTopicSubscriber, needed because
  * Qt's moc and c++ templates don't work nicely together.  Not
  * intended to be used directly. */
-class _AdditionalTopicSubscriber: QObject
+class _AdditionalTopicSubscriber: public QObject // xzt: fixed the incompatibility issue between the latest rviz version and QT
 {
 Q_OBJECT
 public:
